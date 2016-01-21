2d40ef5a60d6db0e863016453b25d559c6b33603
Tamqn <mr.tamqn711@gmail.com>
2016-01-21 16:16:56 +0700

    test sign up

--- StoreManager/AppDelegate.m
+++ StoreManager/AppDelegate.m
@@ -26,96 +26,45 @@ static NSString *storeName = @"StoreManager.sqlite";
     // Override point for customization after application launch.
     
     [Parse setApplicationId:@"Nq0V3ipnh8rQw00UuvDLdI6GmkuXRWAc2WNh9rp2"
-                  clientKey:@"quVE0qbs9OuFRxbhyZ7ysR7PxOO15k25W2ZG7Xh9"];
+                  clientKey:@"nS2i108U6tgX6PtnyGz2bYU7yRmJZlgLty7YrvmO"];
     
-    [PFUser enableAutomaticUser];
+    //1
+    PFUser *user = [PFUser user];
+    //2
+    user.username = @"quachtam";
+    user.password = @"123456789";
+    //3
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            //The registration was successful, go to the wall
+//            [self performSegueWithIdentifier:@"SignupSuccesful" sender:self];
+            
+        } else {
+            //Something bad has occurred
+            NSString *errorString = [[error userInfo] objectForKey:@"error"];
+            UIAlertView *errorAlertView = [[UIAlertView alloc] initWithTitle:@"Error" message:errorString delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+            [errorAlertView show];
+        }
+    }];
     
-    PFACL *defaultACL = [PFACL ACL];
+    [PFUser logInWithUsernameInBackground:@"quachtam" password:@"123" block:^(PFUser *user, NSError *error) {
+        if (user) {
+            //Open the wall
+            [self performSegueWithIdentifier:@"LoginSuccesful" sender:self];
+        } else {
+            //Something bad has ocurred
+            NSString *errorString = [[error userInfo] objectForKey:@"error"];
+            UIAlertView *errorAlertView = [[UIAlertView alloc] initWithTitle:@"Error" message:errorString delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+            [errorAlertView show];
+        }
+    }];
     
-    // If you would like all objects to be private by default, remove this line.
-    defaultACL.publicReadAccess = YES;
-    
-    [PFACL setDefaultACL:defaultACL withAccessForCurrentUser:YES];
-    
-    [MagicalRecord setupCoreDataStackWithAutoMigratingSqliteStoreNamed:storeName];    
+    [MagicalRecord setupCoreDataStackWithAutoMigratingSqliteStoreNamed:storeName];
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     VCRoot *root = [[VCRoot alloc] init];
     [self.window setRootViewController:root];
     [self.window makeKeyAndVisible];
-    
-    
-    if (application.applicationState != UIApplicationStateBackground) {
-        // Track an app open here if we launch with a push, unless
-        // "content_available" was used to trigger a background push (introduced in iOS 7).
-        // In that case, we skip tracking here to avoid double counting the app-open.
-        BOOL preBackgroundPush = ![application respondsToSelector:@selector(backgroundRefreshStatus)];
-        BOOL oldPushHandlerOnly = ![self respondsToSelector:@selector(application:didReceiveRemoteNotification:fetchCompletionHandler:)];
-        BOOL noPushPayload = !launchOptions[UIApplicationLaunchOptionsRemoteNotificationKey];
-        if (preBackgroundPush || oldPushHandlerOnly || noPushPayload) {
-            [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
-        }
-    }
-    
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 80000
-    if ([application respondsToSelector:@selector(registerUserNotificationSettings:)]) {
-        UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
-                                                        UIUserNotificationTypeBadge |
-                                                        UIUserNotificationTypeSound);
-        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
-                                                                                 categories:nil];
-        [application registerUserNotificationSettings:settings];
-        [application registerForRemoteNotifications];
-    } else
-#endif
-    {
-        [application registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge |
-                                                         UIRemoteNotificationTypeAlert |
-                                                         UIRemoteNotificationTypeSound)];
-    }
-    
     return YES;
-}
-
-#pragma mark Push Notifications
-
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
-    [currentInstallation setDeviceTokenFromData:deviceToken];
-    [currentInstallation saveInBackground];
-    
-    [PFPush subscribeToChannelInBackground:@"" block:^(BOOL succeeded, NSError *error) {
-        if (succeeded) {
-            NSLog(@"ParseStarterProject successfully subscribed to push notifications on the broadcast channel.");
-        } else {
-            NSLog(@"ParseStarterProject failed to subscribe to push notifications on the broadcast channel.");
-        }
-    }];
-}
-
-- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
-    if (error.code == 3010) {
-        NSLog(@"Push notifications are not supported in the iOS Simulator.");
-    } else {
-        // show some alert or otherwise handle the failure to register.
-        NSLog(@"application:didFailToRegisterForRemoteNotificationsWithError: %@", error);
-    }
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-    [PFPush handlePush:userInfo];
-    
-    if (application.applicationState == UIApplicationStateInactive) {
-        [PFAnalytics trackAppOpenedWithRemoteNotificationPayload:userInfo];
-    }
-}
-
-///////////////////////////////////////////////////////////
-// this method if you want to use Push Notifications with Background App Refresh
-///////////////////////////////////////////////////////////
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
-    if (application.applicationState == UIApplicationStateInactive) {
-        [PFAnalytics trackAppOpenedWithRemoteNotificationPayload:userInfo];
-    }
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
